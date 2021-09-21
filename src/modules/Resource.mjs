4a244e82b34d034415b1fecbed6cec4d201360fd
superwibr <65673005+superwibr@users.noreply.github.com>
2021-09-21 09:08:51 -0400

    No close

--- src/modules/Resource.mjs
+++ src/modules/Resource.mjs
@@ -69,7 +69,7 @@ Resource.assemble = async function (map, autoUse) {// creates a single document 
 		// restore the document
 		document.append(backup);
 	}
-	document.close()
+	//document.close()
 
 	return new DOMParser().parseFromString(thing, "text/html").documentElement
 }
