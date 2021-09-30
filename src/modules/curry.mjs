d579c0337c87b0db3e129726ad3c2afd360a257e
superwibr <superwibr@gmail.com>
2021-09-29 20:49:40 -0400

    swich => if...else

--- src/modules/curry.mjs
+++ src/modules/curry.mjs
@@ -3,21 +3,19 @@
  * @param yum Sub-function OR function to be curried
  */
 function curry(yum) {
-	switch (yum) {
-		case '':
 
-			break;
+	if (yum == '') {
 
-		default: // smart-currying.
-			return (function curried(...args) {
-				if (args.length >= yum.length) { // if it isn't partial
-					return yum.apply(this, args);
-				} else {
-					return function (...args2) { // if it is partial
-						return curried.apply(this, args.concat(args2));
-					};
+	} else { // smart-currying
+		return function curried(...args) {
+			if (args.length >= yum.length) { // if it isn't partial
+				return yum.apply(this, args);
+			} else {
+				return function (...args2) { // if it is partial
+					return curried.apply(this, args.concat(args2));
 				};
-			});
+			};
+		};
 	}
 }
 
